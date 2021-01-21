3e56b34acabd2ab458df1b8f4313f30b775e9fb0
poteboy <keitatkins4@gmail.com>
2021-01-22 04:22:46 +0900

    roting

--- src/home/header/header.tsx
+++ src/home/header/header.tsx
@@ -2,9 +2,19 @@ import { useRouter } from 'next/router'
 import React, { FC, useState } from 'react'
 import Link from 'next/link'
 
+
 export const Header: FC = () => {
     return (
-        <React.Fragment>
-        </React.Fragment>
+        <header>
+        </header>   
     )
 }
+
+// JSX要素用のスタイル定義（React.CSSProperties形式）
+const style: React.CSSProperties = {
+    color: 'blue',
+    background: '#aaccff',
+    padding: '0.5em',
+    fontWeight: 'bolder',
+    borderRadius: '0.5em',
+  };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React, { useCallback, useState } from 'react';
 import { Header } from '../home/header/header';
 import { Footer } from '../home/footer/footer';
+import { path } from '../config/path';
+import Link from 'next/link';
+
   
 export default function Home() {
     return (
         <React.Fragment>
-            <div>Hello world</div>
+            <Link passHref {...path.home}>
+                <a>To TOP</a>
+            </Link>
             <Header />
             <Footer />
         </React.Fragment>
